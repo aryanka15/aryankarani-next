2797bbdac235e5297f77a091e7d1d1663d91944d
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2023-04-27 17:43:17 -0700

    Made repsonsive layout for blog page

--- app/BlogCard.tsx
+++ app/BlogCard.tsx
@@ -3,9 +3,14 @@
 import NextImage from "next/image";
 
 import BlogData from "./BlogData";
-export default function BlogCard(props: { data: BlogData }) {
+export default function BlogCard(props: { data: BlogData, main: boolean }) {
   const data = props.data;
-
+  let fontSize = 'lg'
+    let background = "blue"
+  if (props.main) {
+    fontSize = 'xl'
+      background = "red"
+  }
   const redirectToBlog = () => {
     window.open(`/blog/${data.id}`, "_blank");
   };
@@ -21,15 +26,15 @@ export default function BlogCard(props: { data: BlogData }) {
           alt="video thumbnail"
         />
       </div>
-      <div className="bg-neutral-400">
-        <h1 className="px-1 py-6 text-sm md:text-lg xl:text-xl font-bold">
+      <div className="flex flex-col justify-center h-[104px] bg-neutral-400">
+        <h1 className={`px-3 py-6 text-sm md:text-lg xl:text-${fontSize} font-bold`}>
           {data.title}
         </h1>
       </div>
       <div className="">
         <button
           onClick={redirectToBlog}
-          className="w-full py-2 border-t-4 border-black bg-red-500"
+          className={`w-full py-2 border-t-4 border-black bg-${background}-500`}
         >
           <a href={`/blog/${data.id}`} target="_blank">
             Read
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,24 +10,180 @@ const bungee = Bungee({
   display: "swap",
 });
 export default function Blog() {
-
   return (
-    <div className={"bg-neutral-700"}>
-      <div className="Blog flex flex-col h-full text-center">
+    <div className={"w-screen bg-neutral-700"}>
+      <div className={`Blog flex flex-col h-full text-center items-center ${bungee.className}   `}>
         <div
-          className={`flex flex-row text-center justify-center text-6xl mt-36 mb-10 ${bungee.className}`}
+          className={`flex flex-col md:flex-row text-center justify-center text-4xl md:text-6xl mt-36 mb-10 ${bungee.className}`}
         >
-          <h1 className={"blog-title-stroke text-neutral-900"}>Aryan Karani -</h1>
-          <h1 className={"blog-subtitle-stroke text-red-500"}>&nbsp;Snapshots</h1>
+          <h1 className={"blog-title-stroke text-neutral-900"}>
+            Aryan Karani's
+          </h1>
+          <h1 className={"blog-subtitle-stroke text-red-500"}>
+            &nbsp;Snapshots
+          </h1>
         </div>
-        <div className="BlogMain grid grid-cols-3 grid-rows-1 gap-x-5 gap-y-5 mx-5 mb-10">
-          <BlogCard data={new BlogData(0, 'The Greatest AI In The World - NoGPT', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
-          <BlogCard data={new BlogData(1, 'sdashdasoud', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
-          <BlogCard data={new BlogData(2, 'sdashdasoud', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
-          <BlogCard data={new BlogData(2, 'sdashdasoud', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
-          <BlogCard data={new BlogData(2, 'sdashdasoud', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
-          <BlogCard data={new BlogData(2, 'sdashdasoud', '/images/MajesticCP.jpg', 'asgduasuhdouas', 'sahdisahdosah')}></BlogCard>
+        <div className="BlogMain grid grid-cols-1 md:grid-cols-3 auto-rows-auto md:grid-rows-1 gap-x-5 gap-y-5 mx-5 mb-10">
+          <BlogCard
+            data={
+              new BlogData(
+                0,
+                "The Greatest AI In The World - NoGPT",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={true}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                1,
+                "sdashdasoud",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={true}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                2,
+                "sdashdasoud",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={true}
+          ></BlogCard>
         </div>
+        <div className="BlogPosts grid grid-cols-2 md:grid-cols-5 grid-rows-3 md:grid-rows-2 gap-x-5 gap-y-5 mx-5 mb-10">
+          <BlogCard
+            data={
+              new BlogData(
+                0,
+                "The Greatest AI In The World - NoGPT",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={false}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                1,
+                "sdashdasoud",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={false}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                2,
+                "sdashdasoud",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={false}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                0,
+                "The Greatest AI In The World - NoGPT",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={false}
+          ></BlogCard>
+          <BlogCard
+            data={
+              new BlogData(
+                0,
+                "The Greatest AI In The World - NoGPT",
+                "/images/MajesticCP.jpg",
+                "asgduasuhdouas",
+                "sahdisahdosah"
+              )
+            }
+            main={false}
+          ></BlogCard>
+            <BlogCard
+                data={
+                    new BlogData(
+                        0,
+                        "The Greatest AI In The World - NoGPT",
+                        "/images/MajesticCP.jpg",
+                        "asgduasuhdouas",
+                        "sahdisahdosah"
+                    )
+                }
+                main={false}
+            ></BlogCard>
+            <BlogCard
+                data={
+                    new BlogData(
+                        0,
+                        "The Greatest AI In The World - NoGPT",
+                        "/images/MajesticCP.jpg",
+                        "asgduasuhdouas",
+                        "sahdisahdosah"
+                    )
+                }
+                main={false}
+            ></BlogCard>
+            <BlogCard
+                data={
+                    new BlogData(
+                        0,
+                        "The Greatest AI In The World - NoGPT",
+                        "/images/MajesticCP.jpg",
+                        "asgduasuhdouas",
+                        "sahdisahdosah"
+                    )
+                }
+                main={false}
+            ></BlogCard>
+            <BlogCard
+                data={
+                    new BlogData(
+                        0,
+                        "The Greatest AI In The World - NoGPT",
+                        "/images/MajesticCP.jpg",
+                        "asgduasuhdouas",
+                        "sahdisahdosah"
+                    )
+                }
+                main={false}
+            ></BlogCard>
+            <BlogCard
+                data={
+                    new BlogData(
+                        0,
+                        "The Greatest AI In The World - NoGPT",
+                        "/images/MajesticCP.jpg",
+                        "asgduasuhdouas",
+                        "sahdisahdosah"
+                    )
+                }
+                main={false}
+            ></BlogCard>
+        </div>
+        <button className={`mb-10 p-3 px-10 text-2xl rounded-xl border-black border-4 bg-blue-500 w-fit hover:bg-red-500 md:focus:bg-red-500 focus:bg-blue-700 transition`}>All Articles</button>
       </div>
     </div>
   );
