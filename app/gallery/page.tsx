302943d7de2cc565c8f6dc9c65baa5cb0a66d1b0
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2023-08-02 13:21:04 +0530

    Only have webp updates, removed gallery updates from this branch

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,20 +1,9 @@
-import PicCard from "../gallery/components/pic-card";
-import PicData from "./PicData";
+
 export default function Gallery() {
   return (
     <>
-      <div className="Gallery flex flex-col h-fit justify-center text-center items-center">
-        <div className={"md:mt-40 mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 content-evenly gap-4"}>
-            <PicCard data={new PicData("0", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshdsabdsabdoashdipsahdiapshdiashfhfdshfdsahfsdiofhsduofhdsouf")}></PicCard>
-            <PicCard data={new PicData("1", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("2", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("3", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("4", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("5", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("6", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("7", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-            <PicCard data={new PicData("8", "/images/MajesticCP.jpg", "Nikon D5600", "naidnasdasd", "hspdijaipdhpashdapshd")}></PicCard>
-        </div>
+      <div className="Gallery flex flex-col h-screen justify-center items-center text-center items-center">
+        <h1 className={"text-4xl"}>In Progress</h1>
       </div>
     </>
   );
