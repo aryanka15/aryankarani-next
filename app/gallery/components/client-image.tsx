9ae2122e0c22cd19231a2a68fdb9ea6d1acbfdbe
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2023-09-06 18:51:00 -0700

    Better UI, changed refresh rate for cache

--- app/gallery/components/client-image.tsx
+++ app/gallery/components/client-image.tsx
@@ -1,39 +1,52 @@
 "use client";
 
-import {CldImage, getCldImageUrl} from "next-cloudinary";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 
 export default function ClientImage(props: {
-    imageData: {
-        id: any;
-        picture: any;
-        width: any;
-        height: any;
-        title: any;
-        description: any;
-        camera: any;
-        location: any;
-    };
+  imageData: {
+    id: any;
+    url: any;
+    picture: any;
+    width: any;
+    height: any;
+    title: any;
+    description: any;
+    camera: any;
+    location: any;
+  };
 }) {
-    const imageData = props.imageData;
-    return (
-        <div className={"flex justify-center w-full mx-10"}>
-            <CldImage priority format={"auto"} overlays={[
-                {
-                    position: {
-                        gravity: "south_east",
-                        x: 0.02,
-                        y: 0.02,
-                    },
-                    text: {
-                        color: "white",
-                        fontFamily: "Roboto",
-                        fontSize: 60,
-                        fontWeight: "regular",
-                        text: "ARYAN KARANI"
-                    }
-                }
-            ]} alt={imageData.description} sizes={"(min-width: 600px) 60vw, 100vw"} src={imageData.id}
-                      width={imageData.width} height={imageData.height}></CldImage>
-        </div>
-    )
+  const imageData = props.imageData;
+  return (
+    <div
+      className={
+        "flex justify-center w-fit mx-10 border-4 shadow-xl border-white/60 rounded-md"
+      }
+    >
+      <CldImage
+        priority
+        format={"auto"}
+        overlays={[
+          {
+            position: {
+              gravity: "south_east",
+              x: 0.02,
+              y: 0.02,
+            },
+            text: {
+              color: "white",
+              fontFamily: "Roboto",
+              fontSize: 60,
+              fontWeight: "regular",
+              text: "ARYAN KARANI",
+            },
+          },
+        ]}
+        alt={imageData.description}
+        sizes={"(min-width: 600px) 60vw, 100vw"}
+        src={imageData.id}
+        width={imageData.width}
+        height={imageData.height}
+      ></CldImage>
+    </div>
+  );
 }
--- app/utils/get-pic-ids.ts
+++ app/utils/get-pic-ids.ts
@@ -1,6 +1,6 @@
 import {cache} from "react";
 import cloudinary from "./cloudinary"
-export const revalidate = 86400; // 24 hours
+export const revalidate = 600; // 24 hours
 export const getPicIds = cache(async () => {
     let results = await cloudinary.v2.search.expression(`folder:gallery/*`).with_field('context').max_results(30).execute()
     return results.resources;
