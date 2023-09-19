cdbec1130a07ec3b5870eec2d7066e5bc23633ec
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2023-09-18 19:38:23 -0700

    To be overwrriten

--- app/utils/get-pic-ids.ts
+++ app/utils/get-pic-ids.ts
@@ -1,6 +1,7 @@
 import {cache} from "react";
 import cloudinary from "./cloudinary"
+export const revalidate = 10;
 export const getPicIds = cache(async () => {
-    let results = await cloudinary.v2.search.expression(`folder:gallery/*`).with_field('context').max_results(30).execute()
+    let results = await cloudinary.v2.search.expression(`folder:gallery/*`).with_field('context').execute()
     return results.resources;
 });
