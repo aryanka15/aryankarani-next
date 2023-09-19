2e74202a1c85f9942e8784930f153f96a8b7ffd1
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2023-09-18 19:44:55 -0700

    No cache

--- app/utils/get-pic-ids.ts
+++ app/utils/get-pic-ids.ts
@@ -1,7 +1,6 @@
 import {cache} from "react";
 import cloudinary from "./cloudinary"
-export const revalidate = 10;
-export const getPicIds = cache(async () => {
+export const getPicIds = async () => {
     let results = await cloudinary.v2.search.expression(`folder:gallery/* AND context:caption`).with_field('context').max_results(30).execute()
     return results.resources;
-});
+};
