bed372a6056801c85935da29db2178e7996d6252
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2024-04-24 11:32:52 -0700

    removed max results from image call to cloudinary

--- app/utils/get-pic-ids.ts
+++ app/utils/get-pic-ids.ts
@@ -1,6 +1,6 @@
 import {cache} from "react";
 import cloudinary from "./cloudinary"
 export const getPicIds = cache(async () => {
-    let results = await cloudinary.v2.search.expression(`folder:gallery/* AND context:caption`).with_field('context').max_results(30).execute()
+    let results = await cloudinary.v2.search.expression(`folder:gallery/* AND context:caption`).with_field('context').execute()
     return results.resources;
 });
