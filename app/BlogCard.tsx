702c7eeba7c2f80dca3e9ab529a692f0887ac43f
Shekhar Karani <aryan.karani2005@gmail.com>
2024-11-29 14:08:09 -0800

    Added sanity

--- app/BlogCard.tsx
+++ app/BlogCard.tsx
@@ -1,44 +1,44 @@
-"use client";
-
 import NextImage from "next/image";
+import type { SanityDocument } from "@sanity/client";
+import Link from "next/link";
 
-import BlogData from "./BlogData";
-export default function BlogCard(props: { data: BlogData, main: boolean }) {
+export default function BlogCard(props: { data: any; main: boolean }) {
   const data = props.data;
-  let fontSize = 'lg'
-    let background = "blue"
+  let fontSize = "lg";
+  let background = "blue";
   if (props.main) {
-    fontSize = 'xl'
-      background = "red"
+    fontSize = "xl";
+    background = "red";
   }
   const redirectToBlog = () => {
-    window.open(`/blog/${data.id}`, "_blank");
+    window.open(`/blog/${data._id}`, "_blank");
   };
 
   return (
     <div className="BlogCard border-4 border-black">
       <div>
-        <NextImage
+        {/* <NextImage
           src={data.thumbnail}
           width={1280}
           height={720}
           className={"aspect-video object-cover border-b-4 border-black"}
           alt="video thumbnail"
-        />
+        /> */}
       </div>
       <div className="flex flex-col justify-center h-[104px] bg-neutral-400">
-        <h1 className={`px-3 py-6 text-sm md:text-lg xl:text-${fontSize} font-bold`}>
+        <h1
+          className={`px-3 py-6 text-sm md:text-lg xl:text-${fontSize} font-bold`}
+        >
           {data.title}
         </h1>
       </div>
       <div className="">
         <button
-          onClick={redirectToBlog}
           className={`w-full py-2 border-t-4 border-black bg-${background}-500`}
         >
-          <a href={`/blog/${data.id}`} target="_blank">
+          <Link href={`/blog/${data.slug}`} target="_blank">
             Read
-          </a>
+          </Link>
         </button>
       </div>
     </div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,21 @@
-"use client";
-
 import { Bungee } from "next/font/google";
-import BlogCard from "../BlogCard";
-import BlogData from "../BlogData";
+import Posts from "./posts";
+import { getPosts } from "../../lib/fetch";
 
 const bungee = Bungee({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
 });
-export default function Blog() {
 
-  
+export default async function Blog() {
+  const posts = await getPosts();
 
   return (
-    <div className={"w-screen bg-neutral-700"}>
-      <div className={`Blog flex flex-col h-full text-center items-center ${bungee.className}   `}>
+    <div className={"w-screen h-screen bg-neutral-700"}>
+      <div
+        className={`Blog flex flex-col h-full text-center items-center ${bungee.className}   `}
+      >
         <div
           className={`flex flex-col md:flex-row text-center justify-center text-4xl md:text-6xl mt-36 mb-10 ${bungee.className}`}
         >
@@ -27,12 +27,16 @@ export default function Blog() {
           </h1>
         </div>
         <div className="BlogMain grid grid-cols-1 md:grid-cols-3 auto-rows-auto md:grid-rows-1 gap-x-5 gap-y-5 mx-5 mb-10">
-            {/*Blog Data-3 latest posts*/}
+          {/*Blog Data-3 latest posts*/}
         </div>
         <div className="BlogPosts grid grid-cols-2 md:grid-cols-5 grid-rows-3 md:grid-rows-2 gap-x-5 gap-y-5 mx-5 mb-10">
-          {/*Blog data-10*/}
+          <Posts posts={posts}></Posts>
         </div>
-        <button className={`mb-10 p-3 px-10 text-2xl rounded-xl border-black border-4 bg-blue-500 w-fit hover:bg-red-500 md:focus:bg-red-500 focus:bg-blue-700 transition`}>All Articles</button>
+        {/* <Link
+          className={`mb-10 p-3 px-10 text-2xl rounded-xl border-black border-4 bg-blue-500 w-fit hover:bg-red-500 md:focus:bg-red-500 focus:bg-blue-700 transition`}
+        >
+          All Articles
+        </Link> */}
       </div>
     </div>
   );
