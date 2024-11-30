6a15dbad3be6118289ba0de642a2861fd8c43a6e
Shekhar Karani <aryan.karani2005@gmail.com>
2024-11-29 20:05:36 -0800

    Hopefully fixed things

--- app/blog/posts.tsx
+++ app/blog/posts.tsx
@@ -1,12 +1,13 @@
 import BlogCard from "../BlogCard";
 
 export default function Posts({ posts }: { posts: any }) {
-  const numberOfPosts = posts.length === 1 ? `1 Post` : `${posts.length}`;
   return (
-    <div>
-      {posts.map((post: any) => (
-        <BlogCard data={post} main={false}></BlogCard>
-      ))}
-    </div>
+    <>
+      <div>
+        {posts.map((post: any) => (
+          <BlogCard data={post} main={false}></BlogCard>
+        ))}
+      </div>
+    </>
   );
 }
