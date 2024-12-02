7b03549f6f970ddebd3722ce3536d4df369a2671
Shekhar Karani <aryan.karani2005@gmail.com>
2024-12-01 17:47:32 -0800

    google search console vertify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
+import { Metadata } from "next";
 import "./global.css";
 import Nav from "./Nav";
 import { clamp } from "./utils/utils";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "ifqq9Z08vZAWb8Z59xVhuA42IfNdrT5-nhyuua35ZlQ",
+  },
+};
 
 export default function RootLayout({
   children,
